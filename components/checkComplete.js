d873643e50715fb1ebc3059ef538e13c04abcded
Roberto Padilla <84651684+RobertoPadilla-dev@users.noreply.github.com>
2023-06-30 12:36:15 -0600

    Update checkComplete.js

--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,10 +1,11 @@
+//Function to validate the task
 const checkComplete = () => {
     const i = document.createElement("i");
     i.classList.add("far", "fa-check-square", "icon");
     i.addEventListener("click", completeTask);
     return i;
   };
-  // IIFE
+
   const completeTask = (event) => {
     const element = event.target;
     element.classList.toggle("fas");
